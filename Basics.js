d6abd818896cd6aa6313bf69b479641e6408b511
Samuel Staines <stuart44.jobs@gmail.com>
2026-04-08 21:14:06 +0530

    Learnt Basics

---
+++ Basics.js
@@ -0,0 +1,20 @@
+// Concatinating Strings
+let firstName = 'Alex';
+let LastName = "Tucker";
+
+let fullName = firstName+" "+LastName;
+console.log(fullName)
+console.log(`${firstName} ${LastName}`)
+
+
+// Even Or Odd Number
+let number = 7;
+if(number%2 == 0){
+    console.log("Even Number");
+}
+else
+    console.log("Odd Number");
+
+
+let row = ""+2+3+4+5;
+console.log(row);

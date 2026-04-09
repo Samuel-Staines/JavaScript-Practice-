f28f58bf495dc3eb9df7741b40b2a2dc39667b42
Samuel Staines <stuart44.jobs@gmail.com>
2026-04-09 23:05:20 +0530

    Learnt more Basics

--- Basics.js
+++ Basics.js
@@ -16,5 +16,36 @@ else
     console.log("Odd Number");
 
 
+// Appending number to a String
 let row = ""+2+3+4+5;
 console.log(row);
+
+
+//understanding Quotes
+let name = 'Ron'
+console.log("My name is ${name}");
+console.log('My name is ${name}');
+console.log(`My name is ${name}`);
+
+
+//understanding operator 
+let expression = (10>5);
+console.log(`${expression}`);
+
+
+//Unserstanding assignment operator
+let val =12, val2 = 12;
+val += val;
+val2 =+ val2;
+console.log(`val1:${val} val2:${val2}`)
+
+
+//Switch
+//firstName="Tod";
+switch(firstName){
+    case 'Alex':
+    case 'Tod':
+        console.log(true); break;
+    default:
+        console.log(false);
+} 
